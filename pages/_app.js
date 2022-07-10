51bf104aa977ebf109a69bc119a5cbd6d2230ffc
Pablo Rodriguez Nunes <pmrn24@gmail.com>
2022-07-10 14:47:16 -0400

    context login

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,50 @@
+import { useMemo, useState, useEffect } from "react";
+import { ToastContainer } from "react-toastify";
+import jwtDecode from "jwt-decode";
+import AuthContext from "../context/AuthContext";
+import { setToken, getToken } from "../api/token";
 import "../scss/global.scss";
 import "semantic-ui-css/semantic.min.css";
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function MyApp({ Component, pageProps }) {
+  const [auth, setAuth] = useState(undefined);
+  const [reloadUser, setReloadUser] = useState(false);
+
+  useEffect(() => {
+    const token = getToken();
+    if (token) {
+      setAuth({
+        token,
+        idUser: jwtDecode(token).id,
+      });
+    } else {
+      setAuth(null);
+    }
+    setReloadUser(false);
+  }, [reloadUser]);
+
+  const login = (token) => {
+    setToken(token);
+    setAuth({
+      token,
+      idUser: jwtDecode(token).id,
+    });
+  };
+
+  const authData = useMemo(
+    () => ({
+      auth,
+      login,
+      logout: () => null,
+      setReloadUser,
+    }),
+    [auth]
+  );
+
+  if (auth === undefined) return null;
   return (
-    <>
+    <AuthContext.Provider value={authData}>
       <Component {...pageProps} />
       <ToastContainer
         position="top-right"
@@ -18,6 +57,6 @@ export default function MyApp({ Component, pageProps }) {
         draggable
         pauseOnHover
       />
-    </>
+    </AuthContext.Provider>
   );
 }
