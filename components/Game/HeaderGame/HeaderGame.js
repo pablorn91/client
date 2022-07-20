c6afa484c9499f9c9678b15488a73f7db7af77bf
Pablo Rodriguez Nunes <pmrn24@gmail.com>
2022-07-20 18:56:11 -0400

    game page InfoGame, CarouselScreenShots

--- components/Game/HeaderGame/HeaderGame.js
+++ components/Game/HeaderGame/HeaderGame.js
@@ -34,10 +34,16 @@ function Info({ game }) {
       />
       <div className="header-game__buy">
         <div className="header-game__buy-price">
-          <p>Precio de venta al publico: {price}$</p>
+          {discount && <p>Precio de venta al publico: {price}$</p>}
           <div className="header-game__buy-price-actions">
-            <p>-{discount}%</p>
-            <p>{price - Math.floor(price * discount) / 100}$</p>
+            {discount ? (
+              <>
+                <p>-{discount}%</p>
+                <p>{price - Math.floor(price * discount) / 100}$</p>
+              </>
+            ) : (
+              <p>{price}$</p>
+            )}
           </div>
         </div>
         <Button className="header-game__buy-btn">Comprar</Button>
